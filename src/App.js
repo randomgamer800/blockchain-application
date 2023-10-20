3b271946009dbd47fc81983f7159c7761e6ef1b9
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2023-10-20 17:35:43 +0800

    2nd iteration of website
    This is the 2nd iteration uploaded on 20/10/23 to Github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import {useState} from 'react';
+import {ethers} from 'ethers';
 import Web3 from "web3";
+import { project_test_ADDRESS, project_test_ABI } from "./contracts/config";
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, useNavigate} from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./components/Navbar";
 import Contact from "./Contact"
 import Exam from "./Exam";
 import DenseTable from "./components/Table";
 
-function App() {
+export default function App() {
+  
   return (
     <div className="App">
       <Navbar/>
@@ -23,4 +27,3 @@ function App() {
   );
 }
 
-export default App;
---
+++ src/contracts/config.js
@@ -0,0 +1,29 @@
+export const project_test_ADDRESS = 0xd9145CCE52D386f254917e481eB44e9943F39138
+export const project_test_ABI = [
+	{
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "x",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
